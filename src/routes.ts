c2ebc03ada22270ce534c4a0b0d285fb3cab1ad4
igordreher <igord.sparrenberger@gmail.com>
2020-10-13 17:07:23 -0300

    Added image upload to orphanage rout

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import OrphanageController from './controllers/OrphanagesController';
 
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
+const upload = multer(uploadConfig);
+
 const routes = Router();
 
-routes.post('/orphanages', OrphanageController.create)
+routes.post('/orphanages', upload.array('images'), OrphanageController.create)
 routes.get('/orphanages', OrphanageController.index)
 routes.get('/orphanages/:id', OrphanageController.show)
 
