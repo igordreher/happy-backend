11ff80d048796e55a048d1341829a5d3018a365a
igordreher <igord.sparrenberger@gmail.com>
2020-10-13 13:34:02 -0300

    Created Orphanage entity

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,38 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './database/models/Orphanage'
 
 import './database/connection'
 
 const app = express();
 app.use(express.json())
 
-app.post('/', (req, res) => {
-    return res.json({message: "Hello World"})
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekend
+    } = req.body;
+
+    const orphanagesRepository = getRepository(Orphanage)
+    
+    const orphanages = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekend
+    });
+
+    await orphanagesRepository.save(orphanages)
+
+    return res.status(201).json(orphanages);
 })
 
 app.listen(3333);
